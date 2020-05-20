1aca7d7da5827d47e17d845a80c2b657303e4736
Rynax0122 <65585472+Rynax0122@users.noreply.github.com>
2020-05-20 17:13:08 +0800

    new projects

---
+++ speedConverterChallenge/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        speedConverterChallenge.toMilesPerHour(10.5);
+        speedConverterChallenge.printConversion(10.5);
+    }
+}
