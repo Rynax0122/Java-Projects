2923bfea4ea3819b96c0b75d8484a7af7851904f
Rynax0122 <65585472+Rynax0122@users.noreply.github.com>
2020-05-21 15:47:09 +0700

    Delete Main.java

--- speedConverterChallenge/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        speedConverterChallenge.toMilesPerHour(10.5);
-        speedConverterChallenge.printConversion(10.5);
-    }
-}
